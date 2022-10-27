e669b60205453f792834e06c6f2652dc57511b37
qiutianhao <>
2022-10-27 17:46:15 +0800

    上传

--- app/src/main/java/com/fk/photogallery/MainActivity.kt
+++ app/src/main/java/com/fk/photogallery/MainActivity.kt
@@ -3,13 +3,27 @@ package com.fk.photogallery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var iamge: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
+        iamge = findViewById(R.id.iv_image)
+
+        button.setOnClickListener() {
+            Glide.with(applicationContext)
+                .load(R.mipmap.icon_placeholder)
+                .placeholder(R.mipmap.icon_accost_list)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(iamge)
+        }
 
     }
 }
--- app/src/main/java/com/fk/photogallery/core/utils/glide/GlideUtil.kt
+++ app/src/main/java/com/fk/photogallery/core/utils/glide/GlideUtil.kt
@@ -1,5 +1,33 @@
 package com.fk.photogallery.core.utils.glide
 
+import android.text.TextUtils
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.fk.photogallery.R
+import com.fk.photogallery.core.application.PhotoGalleryApplication
+import com.fk.photogallery.core.model.RunTimeData
+
 object GlideUtil {
 
+
+
+    fun disPlayWithLocalData(url : String, defaultImage : Int, imageView: ImageView) {
+        RunTimeData.getContext().let {
+
+            TextUtils.isEmpty(url).let {
+                if (it) {
+                    imageView.setImageResource(defaultImage)
+                } else{
+                    Glide.with(RunTimeData.getContext())
+                        .load(url)
+                        .placeholder(defaultImage)
+                        .errorPlaceholder()
+
+                }
+            }
+
+
+        }
+    }
 }
